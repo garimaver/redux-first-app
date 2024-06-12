15d07284d82194e318b156571d317fa594535753
garima verma <114860130+garimaver@users.noreply.github.com>
2024-06-12 19:52:38 +0530

    add actions

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Shop = () => {
   return (
     <div className='container mx-3'>
-      <h2>Buy Adidas shoes 50 rs</h2>
+      <h2>Deposit/Withdraw Money</h2>
       <div className='d-flex mx-3 my-3'>
       <button className='btn  btn-primary mx-3'>-</button>
-      <p className='align-items-center'>Add to cart</p>
+      <p className='align-items-center'>Update Balance</p>
       <button  className='btn  btn-primary mx-3'>+</button>
       </div>
     </div>
